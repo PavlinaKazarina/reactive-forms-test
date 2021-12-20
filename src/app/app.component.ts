cb71a742dc2b466ee55c78acacc19c0de9acd035
PavlinaKazarina <pavlina.kazarina@gmail.com>
2021-12-20 14:13:42 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Validators, FormGroup, FormArray, FormBuilder } from '@angular/forms';
+import { Customer } from './customer.interface';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+})
+export class AppComponent implements OnInit {
+  public myForm: FormGroup; // our form model
+
+  // we will use form builder to simplify our syntax
+  constructor(private _fb: FormBuilder) {}
+
+  ngOnInit() {
+    // we will initialize our form here
+    this.myForm = this._fb.group({
+      name: ['', [Validators.required, Validators.minLength(5)]],
+      addresses: this._fb.array([this.initAddress()]),
+    });
+  }
+
+  initAddress() {
+    // initialize our address
+    return this._fb.group({
+      street: ['', Validators.required],
+      postcode: [''],
+    });
+  }
+
+  addAddress() {
+    // add address to the list
+    const control = <FormArray>this.myForm.controls['addresses'];
+    control.push(this.initAddress());
+  }
+
+  removeAddress(i: number) {
+    // remove address from the list
+    const control = <FormArray>this.myForm.controls['addresses'];
+    control.removeAt(i);
+  }
+}
